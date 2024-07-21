44a6ae9f8c3ad9526b1dffefd92e5ebbdf4ed489
Daniel Toledo <daniel.toledo.fuentessud@gmail.com>
2024-07-20 22:43:13 -0400

    W04 Prove and starting W06 Final project

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -2,8 +2,29 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        List<Video> videos = new List<Video>
+        {
+            new Video("C# Tutorial", "CodeMaster", 3600),
+            new Video("Cooking Tips", "Chef Emily", 1800),
+            new Video("Product Review", "Tech Enthusiast", 900)
+        };
+
+        videos[0].Comments.Add(new Comment("Alice", "Great tutorial!"));
+        // ... Add more comments to each video
+
+        foreach (Video video in videos)
+        {
+            Console.WriteLine($"Title: {video.Title}");
+            Console.WriteLine($"Author: {video.Author}");
+            Console.WriteLine($"Length: {video.LengthInSeconds} seconds");
+            Console.WriteLine($"Comments: {video.GetCommentCount()}");
+
+            foreach (Comment comment in video.Comments)
+            {
+                Console.WriteLine($"- {comment.Author}: {comment.Text}");
+            }
+        }
     }
 }
---
+++ final/Foundation1/Video.cs
@@ -0,0 +1,19 @@
+public class Video
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public int LengthInSeconds { get; set; }
+    public List<Comment> Comments { get; } = new List<Comment>();
+
+    public Video(string title, string author, int lengthInSeconds)
+    {
+        Title = title;
+        Author = author;
+        LengthInSeconds = lengthInSeconds;
+    }
+
+    public int GetCommentCount()
+    {
+        return Comments.Count;
+    }
+}
